c266ec1a2c611307abc94fab1726b819e7a5ddb8
mtgto <hogerappa@gmail.com>
2013-07-22 09:35:35 +0900

    メイン画面の微調整。
    ・メニュー部分とリスト部分をSplitViewで分割
    ・リスト部分の表示サイズをSmallに変更

---
+++ Indexing/MTZeroWidthSplitView.h
@@ -0,0 +1,13 @@
+//
+//  MTZeroWidthSplitView.h
+//  Indexing
+//
+//  Created by mtgto on 7/22/13.
+//  Copyright (c) 2013 mtgto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MTZeroWidthSplitView : NSSplitView
+
+@end
---
+++ Indexing/MTZeroWidthSplitView.m
@@ -0,0 +1,15 @@
+//
+//  MTZeroWidthSplitView.m
+//  Indexing
+//
+//  Created by mtgto on 7/22/13.
+//  Copyright (c) 2013 mtgto. All rights reserved.
+//
+
+#import "MTZeroWidthSplitView.h"
+
+@implementation MTZeroWidthSplitView
+
+- (CGFloat)dividerThickness { return 0.0; }
+
+@end
